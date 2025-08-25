0771f7c9916315f1e2d4b5bac1fcf82c21397c0c
Moaid <moaidnasser19@gmail.com>
2025-08-25 15:29:39 -0700

    Implement kendo grid. And buttons for add and edit products. Also style the product-list component.

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { Product } from '../../models/product';
+import { ButtonModule } from '@progress/kendo-angular-buttons';
+
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [GridModule, ButtonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
 export class ProductList {
-
+ 
+  products: Product[] = [];
+  
 }
