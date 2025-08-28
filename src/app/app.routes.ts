b510eda29e9c6d09d5c4e97d4dbc294933bed1c4
Moaid <moaidnasser19@gmail.com>
2025-08-27 18:35:26 -0700

    Implementaion of edit form with its logic.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { App } from './app';
 import { ProductDetail } from './components/product-detail/product-detail';
+import { EditForm } from './components/edit-form/edit-form';
 
 export const routes: Routes = [
     {path : '' , component : App},
     {path : 'product/:id' , component : ProductDetail},
     {path : 'products' , component : ProductList},
+    {path : 'edit/:id', component : EditForm}
 ];
--- src/app/components/edit-form/edit-form.ts
+++ src/app/components/edit-form/edit-form.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Category, Product } from '../../models/product';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product-service';
+
 
 @Component({
   selector: 'app-edit-form',
-  imports: [],
+  imports: [FormsModule, DropDownsModule],
   templateUrl: './edit-form.html',
   styleUrl: './edit-form.css'
 })
 export class EditForm {
 
+  constructor(private router : ActivatedRoute, private crud : ProductService) {}
+
+  product : Partial<Product> = {}; 
+  categories: Category[] = ['Electronics', 'Clothing', 'Grocery', 'Other'];
+
+  onSubmit (form : any) {
+   console.log(form.value);
+   const id = this.router.snapshot.paramMap.get('id')!;
+   this.crud.updateProduct(id, this.product) 
+
+
+  }
 }
